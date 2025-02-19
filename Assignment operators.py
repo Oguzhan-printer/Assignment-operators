d6b77d5aca11b691009362afefd4797e83a9e344
OĞUZHAN YAZICI <159013707+Oguzhan-printer@users.noreply.github.com>
2025-02-19 09:16:35 +0300

    Add files via upload

---
+++ Assignment operators.py
@@ -0,0 +1,18 @@
+#Task 1
+
+number_of_starts = 10
+
+number_of_starts += 5
+number_of_starts -= 3
+number_of_starts *= 2
+number_of_starts /= 4
+
+print(number_of_starts)
+
+#Task 2
+
+Text = "Python"
+Text += " Programming"
+
+print(Text)
+
